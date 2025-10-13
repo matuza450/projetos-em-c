da0553fa64779ed6ce8b0a97476fa4bc6a6288b8
matuza450 <mattuza@yahoo.com>
2025-10-12 21:02:45 -0300

    update em main.exe

--- main.c
+++ main.c
@@ -5,15 +5,17 @@
 int main(){
 
     float num1, num2, media;
+
     printf("Digite o primeiro numero: ");
-    scanf("%f", num1);
+    scanf("%f", &num1);
 
     printf("Digite o segundo numero: ");
-    scanf("%f", num2);
+    scanf("%f", &num2);
 
-    media = (num1 + num2);
+    media = (num1 + num2)/2;
 
-    printf("Media: %2.f", media);
+    printf("A media dos dois numero e: %2.f", media);
+
 
     return 0;
 }
