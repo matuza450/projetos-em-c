1c52c0c65171cb5a45fb1b355a6eab283b2d54c1
matuza450 <mattuza@yahoo.com>
2025-11-06 06:53:24 -0300

    update

--- teste.cpp
+++ teste.cpp
@@ -73,7 +73,40 @@ diferente), escaleno (todos os lados diferentes) ou eqüilátero (todos os lados
     }
 }
 
+void exercicio3(){
+    /*
+A Secretaria de Meio Ambiente, que controla o índice de poluição, mantém 3 grupos de indústrias
+que são altamente poluentes do meio ambiente. O índice de poluição aceitável varia de 0,05 até 0,29.
+Se o índice sobe para 0,3 as indústrias do 1º grupo são intimadas a suspenderem suas atividades, se o
+índice crescer para 0,4 as indústrias do 1º e 2º grupo são intimadas a suspenderem suas atividades, se
+o índice atingir 0,5 todos os grupos devem ser notificados a paralisarem suas atividades. Faça um
+programa que leia o índice de poluição
+    */
 
+    float indicePoluicao;
+
+    printf("Digite o índice de Poluíção: \n");
+    scanf("%f", &indicePoluicao);
+
+    if (indicePoluicao >= 0.5) {
+        printf("Notificação: Todos os 3 grupos devem suspender as atividades.\n");
+
+    } else if (indicePoluicao >= 0.4){
+        printf("Notificação: Indústrias do 1° e 2° grupo devem suspender as atividades. \n");
+
+    } else if (indicePoluicao >= 0.3) {
+        printf("Noificação: Indústrias do 1° grupo devem suspender as atividades. \n");
+    } else if (indicePoluicao >= 0.05) {
+        printf("Situação: Índice de poluição está em nível aceitável. \n");
+
+    } else {
+        printf("Situação: Índice abaixo do nível de monitoramento. \n");
+
+    }
+
+    return;
+
+}
 
 
 int main(){
@@ -87,6 +120,7 @@ int main(){
         printf("Escolha um exercício para executar:\n");
         printf("1 - Exercício 01\n"); 
         printf("2 - Exercício 02\n");
+        printf("3 - Exercício 03\n");
         printf("0 - Sair\n");         
         printf("Digite sua opção: ");
 
@@ -104,6 +138,10 @@ int main(){
 
             case 2:
                 exercicio2();
+                break;
+
+            case 3:
+                exercicio3();
                 break;
 
             case 0: 
